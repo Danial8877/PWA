db074e5f1ee681c05ac6cd0e3170e9dc0cb9792b
danial <138702391+Danial8877@users.noreply.github.com>
2025-07-09 15:44:05 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,6 @@
 //   "/",
 //   "/index.html",
 //   "/offline.html",
-//   "/assets/css/style.css",
 //   "/assets/pwa/pwa.js",
 //   "/assets/images/icon.png",
 //   "/assets/pwa/manifest.json",
@@ -44,12 +43,6 @@ const STATIC_CACHE = `static-v${VERSION}`;
 const DYNAMIC_CACHE = `dynamic-v${VERSION}`;
 const FONT_CACHE = `font-cache-v${VERSION}`;
 const API_CACHE = `api-cache-v${VERSION}`;
-workbox.core.setCacheNameDetails({
-  prefix: "my-app",
-  suffix: VERSION,
-  precache: "precache",
-  runtime: "runtime",
-});
 self.skipWaiting();
 workbox.core.clientsClaim();
 workbox.precaching.cleanupOutdatedCaches();
